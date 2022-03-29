498520f5cae82b504d53994ae123fa09e3e8ee4e
Edimar Rodrigues Júnior <rodriguesjunior.edimar@gmail.com>
2022-03-28 21:07:31 -0400

    Update repository

---
+++ ProjetoElevador/ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    public class Elevador
+    {
+        private int capacidadeMax;
+        private int ttAndares;
+        private int quantPessoas = 0;
+        private int andarAtual = 1;
+
+        public void Inicializar(int capacidade, int andares)
+        {
+            this.capacidadeMax = capacidade;
+            this.ttAndares = andares;
+        }
+
+        public void Entrar()
+        {
+            if (this.quantPessoas == this.capacidadeMax)
+            {
+                Console.WriteLine("CAPACIDADE MÁXIMA ATINGIDA");
+            }else
+            {
+                this.quantPessoas += 1;
+                Console.WriteLine($"Pessoas no Elevador: {this.quantPessoas}");
+            }
+        }
+
+        public void Sair()
+        {
+            if (this.quantPessoas == 0)
+            {
+                Console.WriteLine("ELEVADOR VAZIO");
+            }else
+            {
+                this.quantPessoas -= 1;
+                Console.WriteLine($"Pessoas no Elevador: {this.quantPessoas}");
+            }
+        }
+
+        public void Subir()
+        {
+            if (this.andarAtual == this.ttAndares)
+            {
+                Console.WriteLine("LIMITE ATINGIDO");
+            }else
+            {
+                this.andarAtual += 1;
+                Console.WriteLine($"Andar Atual: {this.andarAtual}");
+            }
+        }
+
+        public void Descer()
+        {
+            if (this.andarAtual == 1)
+            {
+                Console.WriteLine("NÃO EXISTE ANDARES ABAIXO");
+            } else
+            {
+                this.andarAtual += 1;
+                Console.WriteLine($"Andar Atual: {this.andarAtual}");
+            }
+        }
+
+    }
+}
---
+++ ProjetoElevador/ProjetoElevador/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using ProjetoElevador.Model;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevador elevador = new Elevador();
+
+            Console.Write("Capacidade do Elevador: ");
+            int capacidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Total de Andares: ");
+            int andares = int.Parse(Console.ReadLine());
+
+            elevador.Inicializar(capacidade, andares);
+
+            Console.WriteLine();
+
+            while(true)
+            {
+                Console.WriteLine("1 - Entrar\n2 - Sair\n3 - Subir\n4 - Descer\n5 - Sair do Programa");
+                Console.Write("Escolha uma opção: ");
+                int escolha = int.Parse(Console.ReadLine());
+
+                if (escolha == 5) {
+                    break;
+                }
+
+                switch(escolha)
+                {
+                    case 1:
+                        elevador.Entrar();
+                        break;
+                    case 2:
+                        elevador.Sair();
+                        break;
+                    case 3:
+                        elevador.Subir();
+                        break;
+                    case 4:
+                        elevador.Descer();
+                        break;
+                    default:
+                        Console.WriteLine("ESCOLHA INVÁLIDA!");
+                        break;
+                }
+            }
+        }
+    }
+}
